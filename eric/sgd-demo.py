624b46e6e6967d969e03db58aa210eb968e7a685
Eric Liang <ekhliang@gmail.com>
2018-04-11 21:58:52 -0700

    updates

--- eric/sgd-demo.py
+++ eric/sgd-demo.py
@@ -90,7 +90,6 @@ class SGDWorker(object):
        # trace_file.write(trace.generate_chrome_trace_format())
 
     def compute_gradients(self):
-        tf.control_dependencies[self.device_grads_and_vars]
         fetches = self.sess.run(
             [g for g, v in self.device_grads_and_vars],
             feed_dict=self.feed_dict())
