bb40b6a71b280a0d4a6240e66022f2b3eb31f471
Ubuntu <ubuntu@ip-172-31-79-123.ec2.internal>
2018-04-12 00:47:41 +0000

    multi gpu

--- eric/sgd-demo.py
+++ eric/sgd-demo.py
@@ -10,7 +10,6 @@ import ray
 import time
 
 
-
 class SimpleModel(object):
     def __init__(self):
         self.inputs = tf.placeholder(shape=[None, 4], dtype=tf.float32)
@@ -38,37 +37,70 @@ class TFBenchModel(object):
         logits, aux = self.model.build_network(self.inputs, data_format="NHWC")
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=self.labels)
         self.loss = tf.reduce_mean(loss, name='xentropy-loss')
+        self.optimizer = tf.train.AdamOptimizer()
 
     def feed_dict(self):
         return {
             self.inputs: np.zeros(self.inputs.shape.as_list()),
             self.labels: np.zeros(self.labels.shape.as_list())
         }
+# import ipdb
+# from tensorflow.python.client import timeline
+# def debug():
+#     return ipdb.set_trace(context=5)
 
 
-@ray.remote(num_gpus=1)
+
 class SGDWorker(object):
     def __init__(self, i, model_cls, all_reduce_alg=None, num_gpus=1):
+        # TODO - just port VariableMgrLocalReplicated
         self.i = i
+        assert num_gpus > 0
         self.sess = tf.Session()
-        self.model = model_cls()
-        self.optimizer = tf.train.AdamOptimizer()
-        if all_reduce_alg:
-            # allreduce.sum_gradients_all_reduce()
-            grads_and_vars = allreduce.sum_grad_and_var_all_reduce(
-                grads_and_vars, 1, all_reduce_alg, list(range(num_gpus)))
+        models = []
+        grad_ops = []
 
-        self.grad_op = self.optimizer.compute_gradients(self.model.loss)
-        self.apply_op = self.optimizer.apply_gradients(self.grad_op)
+        for device_idx in range(num_gpus):
+            with tf.device("/gpu:%d" % device_idx):
+                with tf.variable_scope("device_%d" % device_idx):
+                    model = model_cls()
+                    models += [model]
+                    model.grads = [t for t in model.optimizer.compute_gradients(model.loss) if t[0] is not None]
+                    grad_ops.append(model.grads)
+
+        self.models = models
+        if num_gpus == 1:
+           grad_ops = grad_ops[0] 
+        elif all_reduce_alg:
+            grad_ops = allreduce.sum_gradients_all_reduce(
+                "", grad_ops, 1, all_reduce_alg, 1, list(range(num_gpus)))
+
+        self.grad_op = grad_ops
+        self.apply_op = tf.group(
+            *[m.optimizer.apply_gradients(m.grads) for m in models])
         self.sess.run(tf.global_variables_initializer())
 
+    def feed_dict(self):
+        result = {}
+        for m in self.models:
+            result.update(m.feed_dict())
+        return result 
+
     def compute_apply(self):
-        self.sess.run(self.apply_op, feed_dict=self.model.feed_dict())
+       # import os
+       # run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+       # run_metadata = tf.RunMetadata()
+       self.sess.run(self.apply_op, feed_dict=self.feed_dict(),)
+           options=run_options,
+           run_metadata=run_metadata)
+       # trace = timeline.Timeline(step_stats=run_metadata.step_stats)
+       # trace_file = open("/tmp/timeline-load.json", "w")
+       # trace_file.write(trace.generate_chrome_trace_format())
 
     def compute_gradients(self):
         l, g = self.sess.run(
-            [self.model.loss, [g[0] for g in self.grad_op]],
-            feed_dict=self.model.feed_dict())
+            [self.models[0].loss, [g[0] for g in self.grad_op]],
+            feed_dict=self.feed_dict())
         return l, g
 
     def apply_gradients(self, grads):
@@ -86,24 +118,31 @@ def average_gradients(grads):
 
 
 # TODO(ekl) replace with allreduce
+
 def do_sgd_step(actors):
-#    ray.get([a.compute_apply.remote() for a in actors])
-    grads = ray.get([a.compute_gradients.remote() for a in actors])
-    print("Avg loss", np.mean([l for (l, g) in grads]))
-    avg_grad = average_gradients([g for (l, g) in grads])
-    for a in actors:
-        a.apply_gradients.remote(avg_grad)
+    ray.get([a.compute_apply.remote() for a in actors])
+    # grads = ray.get([a.compute_gradients.remote() for a in actors])
+    # print("Avg loss", np.mean([l for (l, g) in grads]))
+    # avg_grad = average_gradients([g for (l, g) in grads])
+    # for a in actors:
+    #     a.apply_gradients.remote(avg_grad)
 
 
 if __name__ == "__main__":
     ray.init()
-    # from resnet_demo import RayModel
+    #worker = SGDWorker(0, TFBenchModel, 'nccl', 2)
+    #g = worker.compute_apply()
+
+    #import ipdb; ipdb.set_trace()
 
     model = TFBenchModel
-    n = 8
-    actors = [SGDWorker.remote(i, model) for i in range(n)]
-    for i in range(100):
+    n = 1
+    import sys
+    gpus = int(sys.argv[1])
+    RemoteSGDWorker = ray.remote(num_gpus=gpus)(SGDWorker)
+    actors = [RemoteSGDWorker.remote(i, model, 'nccl', num_gpus=gpus) for i in range(n)]
+    for i in range(5):
         start = time.time()
         print("Distributed sgd step", i)
         do_sgd_step(actors)
-        print("Images per second", 64 * n / (time.time() - start))
+        print("Images per second", 64 * n * gpus / (time.time() - start))
