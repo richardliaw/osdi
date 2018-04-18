c6e8a0db043506aad2285744fef81f4e1cfaaf14
Richard Liaw <rliaw@berkeley.edu>
2018-04-18 13:34:41 -0700

    try this

--- tfbench/modified_allreduce.py
+++ tfbench/modified_allreduce.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 
 import collections as pycoll
 import re
+import numpy as np
 
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
@@ -83,3 +84,58 @@ def sum_gradients_all_reduce(dev_prefixes,
     group_index = (group_index + 1) % len(aux_device_groups)
   new_tower_grads = [list(x) for x in zip(*reduced_gv_list)]
   return new_tower_grads, packing
+
+
+
+def pack_small_tensors(tower_grads, max_bytes=0):
+  """Concatenate gradients together in a more intelligent manner.
+
+  Args:
+    tower_grads: List of lists of (gradient, variable) tuples.
+    max_bytes: Int giving max number of bytes in a tensor that
+      may be considered small.
+    max_group: Int giving max number of small tensors that may be
+      concatenated into one new tensor.
+  """
+  assert max_bytes >= 0
+  orig_grads = [g for g, _ in tower_grads[0]]
+  # Check to make sure sizes are accurate; not entirely important
+  assert all(g.dtype == tf.float32 for g in orig_grads)
+  sizes = [4 * g.shape.num_elements() for g in orig_grads]
+  print("Before packing - avg size %f, median size %f" % (
+    np.mean(sizes), np.median(sizes)))
+  small_ranges = []
+  large_indices = []
+
+  def end_interval(indices):
+    if len(indices) > 1:
+      small_ranges += [[indices[0], indices[-1]]]
+    else:
+      large_indices += indices
+
+  cur_range = []
+  cur_size = 0
+  for i, s in enumerate(sizes):
+    if cur_size > max_bytes:
+      end_interval(cur_range)
+      cur_range = []
+      cur_size = 0
+    cur_range += [i]
+    cur_size += s
+  end_interval(cur_range)
+
+  if len(small_ranges):
+    new_tower_grads = []
+    for dev_idx, gv_list in enumerate(tower_grads):
+      assert len(gv_list) == num_gv
+      new_gv_list = []
+      for r in small_ranges:
+        key = '%d:%d' % (dev_idx, len(new_gv_list))
+        new_gv_list.append((pack_range(key, packing, gv_list, r),
+                            'packing_var_placeholder'))
+      for i in large_indices:
+        new_gv_list.append(gv_list[i])
+      new_tower_grads.append(new_gv_list)
+    return new_tower_grads, packing
+  else:
+    return tower_grads, None
