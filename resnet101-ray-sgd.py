2d467092b8680a2f0e567b323af22b92b3fff103
Eric Liang <ekhliang@gmail.com>
2018-04-20 14:05:39 -0700

    add prefetch ops

--- resnet101-ray-sgd.py
+++ resnet101-ray-sgd.py
@@ -294,6 +294,9 @@ class ParameterServer(object):
         self.accumulated = np.zeros(shard_shape, dtype=np.float32)
         self.acc_counter = 0
 
+    def prefetch(self, oids):
+        fetch(oids)
+
     def add(self, grad_shard_id):
         fetch([grad_shard_id])
         oid = ray.pyarrow.plasma.ObjectID(grad_shard_id)
@@ -384,6 +387,13 @@ def distributed_sgd_step(actors, ps_list, args):
     for actor, grad_shard_oids in zip(actors, grad_shard_oids_list):
         run = actor.ps_compute_apply.remote(grad_shard_oids, accum_shard_ids)
         runs.append(run)
+
+    # Issue prefetch ops
+    for j, (ps, weight_shard_oid) in enumerate(zip(ps_list, accum_shard_ids)):
+        to_fetch = []
+        for grad_shard_oids in grad_shard_oids_list:
+            to_fetch.append(grad_shard_oids[j])
+        ps.prefetch.remote(to_fetch)
 
     # Aggregate the gradients produced by the actors. These operations
     # run concurrently with the actor methods above.
