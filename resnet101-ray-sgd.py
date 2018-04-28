322754cd1d4d3d6dcf046f1598388993640c7bbe
Eric Liang <ekhliang@gmail.com>
2018-04-27 19:50:33 -0700

    add torch import

--- resnet101-ray-sgd.py
+++ resnet101-ray-sgd.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
 import random
 import numpy as np
 import tensorflow as tf
